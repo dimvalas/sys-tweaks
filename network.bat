486ae2e75f982ecc2230f7e1666cfec38bc42041
val <dimitrisvalaselliss@gmail.com>
2025-03-26 18:30:01 +0200

    Add files via upload

---
+++ network.bat
@@ -0,0 +1,43 @@
+echo Optimizing maximum connections per 1.0 server for Explorer and Internet Explorer
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Internet Explorer\MAIN\FeatureControl\FEATURE_MAXCONNECTIONSPER1_0SERVER" /v "explorer.exe" /t REG_DWORD /d 10 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Internet Explorer\MAIN\FeatureControl\FEATURE_MAXCONNECTIONSPER1_0SERVER" /v "iexplore.exe" /t REG_DWORD /d 10 /f
+echo Optimizing maximum connections per server for Explorer and Internet Explorer
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Internet Explorer\MAIN\FeatureControl\FEATURE_MAXCONNECTIONSPERSERVER" /v "explorer.exe" /t REG_DWORD /d 10 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Internet Explorer\MAIN\FeatureControl\FEATURE_MAXCONNECTIONSPERSERVER" /v "iexplore.exe" /t REG_DWORD /d 10 /f
+echo Optimizing DNS, Local, Hosts, and NetBT priority
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\ServiceProvider" /v "DnsPriority" /t REG_DWORD /d 6 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\ServiceProvider" /v "LocalPriority" /t REG_DWORD /d 4 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\ServiceProvider" /v "HostsPriority" /t REG_DWORD /d 5 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\ServiceProvider" /v "NetbtPriority" /t REG_DWORD /d 7 /f
+echo Optimizing network throttling index and system responsiveness
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /v "NetworkThrottlingIndex" /t REG_DWORD /d 0xffffffff /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /v "SystemResponsiveness" /t REG_DWORD /d 0 /f
+echo Optimizing MaxUserPort, TcpTimedWaitDelay, and DefaultTTL
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "MaxUserPort" /t REG_DWORD /d 65534 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "TcpTimedWaitDelay" /t REG_DWORD /d 30 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "DefaultTTL" /t REG_DWORD /d 64 /f
+echo Optimizing PowerShell TCP
+PowerShell.exe Set-NetTCPSetting -SettingName internet -AutoTuningLevelLocal disabled
+PowerShell.exe Set-NetTCPSetting -SettingName internet -ScalingHeuristics disabled
+PowerShell.exe Set-NetTcpSetting -SettingName internet -EcnCapability enabled
+PowerShell.exe Set-NetTcpSetting -SettingName internet -Timestamps enabled
+PowerShell.exe Set-NetTcpSetting -SettingName internet -MaxSynRetransmissions 2
+PowerShell.exe Set-NetTcpSetting -SettingName internet -NonSackRttResiliency disabled
+PowerShell.exe Set-NetTcpSetting -SettingName internet -InitialRto 2000
+PowerShell.exe Set-NetTcpSetting -SettingName internet -MinRto 300
+echo Optimizing PowerShell offload and network
+PowerShell.exe Set-NetOffloadGlobalSetting -ReceiveSegmentCoalescing disabled
+PowerShell.exe Set-NetOffloadGlobalSetting -ReceiveSideScaling disabled
+PowerShell.exe Set-NetOffloadGlobalSetting -Chimney disabled
+PowerShell.exe Disable-NetAdapterLso -Name *
+PowerShell.exe Disable-NetAdapterChecksumOffload -Name *
+echo Optimizing netsh commands TCP & MTU
+netsh int tcp set supplemental internet congestionprovider=ctcp
+netsh interface ipv4 set subinterface "Wi-Fi" mtu=1500 store=persistent
+netsh interface ipv6 set subinterface "Wi-Fi" mtu=1500 store=persistent
+netsh interface ipv4 set subinterface "Ethernet" mtu=1500 store=persistent
+netsh interface ipv6 set subinterface "Ethernet" mtu=1500 store=persistent
+echo Registry tweaks & Network tweaks are done, please reboot your PC for them to apply properly.
+exit
+
+
